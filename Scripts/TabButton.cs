1d4df03398ddbcb929e5e70e87de78fdc8599809
Coem <Kevin.wenten@gmx.de>
2020-07-02 19:19:06 +0200

    Simple tab system for unity

---
+++ Scripts/TabButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Image))]
+public class TabButton : MonoBehaviour ,IPointerClickHandler,IPointerEnterHandler , IPointerExitHandler
+{
+    public TabGroup tabGroup;
+    public Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+        tabGroup.Addbutton(this);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        tabGroup.onClickButton(this);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tabGroup.onEnterButton(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tabGroup.onExitButton(this);
+    }
+}
---
+++ Scripts/TabGroup.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabGroup : MonoBehaviour
+{
+    public TabButton selectedTab;
+    public List<TabButton> tabs;
+
+    public List<GameObject> pages = new List<GameObject>(); 
+
+
+    public Sprite offImage;
+    public Sprite hoverImage;
+    public Sprite activeImage;
+    public void Addbutton(TabButton button)
+    {
+        if (tabs.Count <= 0)
+        {
+            tabs = new List<TabButton>();
+            selectedTab = button;
+            button.image.sprite = activeImage;
+        }
+        tabs.Add(button);
+    }
+
+    public void onClickButton(TabButton button)
+    {
+        selectedTab = button;
+
+        for (int i = 0; i < tabs.Count; i++)
+        {
+            if (tabs[i] != button)
+            {
+                tabs[i].image.sprite = offImage;
+                pages[i].SetActive(false);
+            }
+            else
+            {
+                pages[i].SetActive(true);
+            }
+        }
+
+        selectedTab.image.sprite = activeImage;
+    }
+
+    public void onEnterButton(TabButton button)
+    {
+        if (button != selectedTab)
+        {
+            button.image.sprite = hoverImage;
+        }
+    }
+
+    public void onExitButton(TabButton button)
+    {
+        if (button != selectedTab)
+        {
+            button.image.sprite = offImage;
+        }
+    }
+}
