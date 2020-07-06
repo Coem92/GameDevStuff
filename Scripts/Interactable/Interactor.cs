7c84848b7eea7988643edc695f2744a93f4e1984
Coem <Kevin.wenten@gmx.de>
2020-07-06 16:40:03 +0200

    Interactable

---
+++ Scripts/Interactable/Interactor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactor : MonoBehaviour
+{
+    public float rayLength = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, rayLength))
+        {
+            Interactable interactable = hit.transform.GetComponent<Interactable>();
+            if (interactable != null)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    interactable.Action();
+                }
+            }
+        }
+    }
+}
