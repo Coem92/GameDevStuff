7213cb576ee67888b0984ca8240b38dbc13618f9
Coem <Kevin.wenten@gmx.de>
2020-07-06 15:29:56 +0200

    added player movent

---
+++ Scripts/Player movment/CameraRotator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotator : MonoBehaviour
+{
+    public float sensitivity = 10f;
+    public float maxYAngle = 80f;
+    private Vector2 currentRotation;
+    void Update()
+    {
+        currentRotation.x += Input.GetAxis("Mouse X") * sensitivity;
+        currentRotation.y -= Input.GetAxis("Mouse Y") * sensitivity;
+        currentRotation.x = Mathf.Repeat(currentRotation.x, 360);
+        currentRotation.y = Mathf.Clamp(currentRotation.y, -maxYAngle, maxYAngle);
+        transform.parent.rotation = Quaternion.Euler(0,currentRotation.x, 0);
+        transform.localRotation = Quaternion.Euler(currentRotation.y, 0, 0);
+
+    }
+}
---
+++ Scripts/Player movment/WASDPlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class WASDPlayerController : MonoBehaviour
+{
+
+    public float speed = 1;
+    Rigidbody rigidbody;
+    Vector3 moveVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        setRigidboby();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveVector = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveVector += transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveVector -= transform.forward;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveVector += transform.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveVector -= transform.right;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
+        rigidbody.MovePosition(transform.position+moveVector*speed*Time.fixedDeltaTime);
+    }
+
+    void setRigidboby()
+    {
+        rigidbody.constraints =  RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+    }
+}
