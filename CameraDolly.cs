0d3277dde6012e3ec84b2753bfaa653759e068a3
Coem <Kevin.wenten@gmx.de>
2020-07-15 23:41:10 +0200

    Add files via upload

---
+++ CameraDolly.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDolly : MonoBehaviour
+{
+    public Transform target;
+    public float sensitivity = 10;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position;
+        if(Input.GetMouseButton(0)){ 
+        transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivity, 0);
+        }
+    }
+}
---
+++ ThridPersonCharacterController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThridPersonCharacterController : MonoBehaviour
+{
+
+    public CharacterController controller;
+    public Transform cam;
+    public float speed = 5;
+    float turnSoomthTime = 0.1f;
+
+    float turnSoomthVelocity;
+
+    private float gravitySpeed = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+        Vector3 movedir = Vector3.zero;
+        if (direction.sqrMagnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSoomthVelocity, turnSoomthTime);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            movedir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+        }
+            movedir = movedir.normalized * speed ;
+
+            if (controller.isGrounded)
+            {
+                gravitySpeed = 0;
+            }
+            else
+            {
+                gravitySpeed -= 98.1f*Time.deltaTime;
+                
+            }
+            movedir.y = gravitySpeed;
+
+            controller.Move(movedir * Time.deltaTime);
+
+            
+        
+    }
+}
